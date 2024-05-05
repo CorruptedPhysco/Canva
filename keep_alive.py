f4ad16df714e5df3402b83b21bcd744bd53cbe53
CorruptedPhysco <106505409+CorruptedPhysco@users.noreply.github.com>
2024-05-05 14:21:55 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,31 @@
 from flask import Flask,render_template
-
+import requests 
+from bs4 import BeautifulSoup
 from threading import Thread
 
 app = Flask(__name__)
 
+
+
+def getlink():
+  url="https://bingotingo.com/best-social-media-platforms/"
+  page=requests.get(url)
+  html=page.content
+  soup = BeautifulSoup(html,"html.parser") 
+  job_desc = soup.find(
+    'a', 
+    class_='su-button su-button-style-soft su-button-wide')
+  l = str(job_desc)
+  list = l.split()
+  for i in list:
+    if "href" in i:
+      j = (i.lstrip('href='))
+      job_desc=j.replace('"', '')
+  return job_desc
+ 
 @app.route('/')
 def index():
-  return "alive"
+  return getlink()
 
 
 def run():
