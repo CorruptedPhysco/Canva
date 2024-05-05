7fbc1190b34a46d64b48f8288c4f3f95142394da
CorruptedPhysco <106505409+CorruptedPhysco@users.noreply.github.com>
2024-05-05 14:18:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,6 @@ def getlink():
   return job_desc
  
 
-
-getlink()
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
